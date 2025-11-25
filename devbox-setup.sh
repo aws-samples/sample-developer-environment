1aad216f66500dbfefcd9c2c20dee48caf941cfe
Mo Mohoboob <momohob@amazon.com>
2025-11-25 20:42:03 +0000

    Remove systemctl isolate - causes indefinite hang
    systemctl isolate graphical.target hangs when run from UserData
    Desktop will start on next boot after set-default

--- devbox-setup.sh
+++ devbox-setup.sh
@@ -343,7 +343,6 @@ if [ "${ENABLE_KIRO_IDE}" = "true" ] && [ "${INSTANCE_ARCHITECTURE}" = "amd64" ]
     dnf groupinstall -y "Desktop"
     dnf upgrade -y
     systemctl set-default graphical.target
-    systemctl isolate graphical.target
     mkdir -p /home/ec2-user/.config
     touch /home/ec2-user/.config/gnome-initial-setup-done
     chown -R ec2-user:ec2-user /home/ec2-user/.config
