754b937a16a2ee62665267df28b7f908bcc5de7b
Mo Mohoboob <momohob@amazon.com>
2025-11-26 08:38:03 +0000

    Add systemctl start gdm to launch desktop environment
    - Desktop environment was installed but GDM wasn't starting automatically
    - Added explicit systemctl start gdm after DCV server start
    - This ensures GNOME desktop is available for DCV sessions

--- devbox-setup.sh
+++ devbox-setup.sh
@@ -343,6 +343,7 @@ if [ "${ENABLE_KIRO_IDE}" = "true" ] && [ "${INSTANCE_ARCHITECTURE}" = "amd64" ]
     dnf groupinstall -y "Desktop"
     dnf upgrade -y
     systemctl set-default graphical.target
+    systemctl start gdm
     mkdir -p /home/ec2-user/.config
     touch /home/ec2-user/.config/gnome-initial-setup-done
     chown -R ec2-user:ec2-user /home/ec2-user/.config
@@ -420,6 +421,9 @@ DCVEOF
     # Enable and start DCV server
     systemctl enable dcvserver
     systemctl start dcvserver
+    
+    # Start GNOME Display Manager for desktop environment
+    systemctl start gdm
     ' "Failed to install NICE DCV"
 
     # Install xdg-utils and configure Firefox as default browser for Kiro IDE authentication
